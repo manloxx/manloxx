d2bf57bfce1c7c25792520733245c236ea36ec14
manloxx1337 <vorobeyjack460@gmail.com>
2024-09-09 19:12:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+﻿	<!DOCTYPE html>
+	<html lang="en" style="--gradient-color: linear-gradient(330deg, rgb(112, 1, 223) 0%, rgb(36, 0, 179) 100%); --gradient-hover-color: linear-gradient(330deg, rgb(87, 0, 173) 0%, rgb(26, 0, 128) 100%); --gradient-focus-color: linear-gradient(330deg, rgb(64, 0, 128) 0%, rgb(25, 0, 127) 100%); --mouse-x: 1909px; --mouse-y: 1px;">
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<script src="jquery-3.6.4.min.js"></script>
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<meta name="keywords" content="Wissend, Wissend nextgen, nurik, wexside, celestial, akrien, minecraft, minecraft 1.8, minecraft 1.12.2, minecraft 1.16.5, deadcode, нурсултан, Wissend client, нурсултан клиент, нурик, вексайд, целестиал, акриен, майнкрафт, чит, читы, читы для майнкрафт, читы для minecraft, чит на майнкрафт, чит на minecraft">
+		<meta name="anypay-verification" content="9d4424bc46b7fe4b4da6fe282eb3">
+		<meta name="description" content="Manloxx Client - Лучший чит-клиент для комфортной игры. Для майнкрафт 1.16.5">
+		<title id="title">Manloxx - Лучший клиент для комфортной игры.</title>
+		<link href="assets/css-recode/main/all.min.css" rel="stylesheet" crossorigin="use-credentials">
+		<link href="assets/css-recode/main/animate.min.css" rel="stylesheet">
+		<script defer="defer" src="assets/css-recode/main/main.ac9aa76c.js"></script>
+		<script src="assets/css-recode/main/767b1751eb.js" crossorigin="anonymous"></script>
+		<link href="assets/css-recode/all3/boxicons.min.css" rel="stylesheet">
+		<link href="assets/css/fontmanager.css" rel="stylesheet">
+		<link href="assets/css-recode/all3/main.652df174.css" rel="stylesheet"> </head>
+
+	<body>
+		<noscript>Вам нужно включить JavaScript, чтобы пользоваться сайтом.</noscript>
+		<div id="root">
+			
+<nav class="NavBar_navBarScroll__qoIDu navbar navbar-expand-md fixed-top" id="header">
+	<div class="container-xl">
+		<a class="navbar-brand mr-2" href="index.php.html">
+			<div class="logo mr-2" style="background-image: url(&quot;assets/img/logo.png&quot;);"></div>
+			Manloxx<span>0.2</span>
+		</a>
+		<button aria-controls="navbar" type="button" aria-label="Toggle navigation" class="navbar-toggler collapsed"><span class="navbar-toggler-icon"></span></button>
+		<div class="navbar-collapse collapse" id="navbar">
+			<div class="navbar-nav">
+				<div class="mr-3 dropdown">
+				</div>
+				<hr class="mt-1 mb-1 d-md-none">
+			</div>
+			<div class="ml-auto navbar-nav">
+				<hr class="mt-1 mb-3 d-md-none"><a class="nav-link" href="authorization.php.html">
+            <i class="fa-solid fa-bag-shopping mr-2" aria-hidden="true"></i>Купить</a>
+                <a class="btn btn-lg btn-gradient" href="authorization.php.html"><i class="fa-solid fa-right-to-bracket mr-2"></i>Авторизация</a>			</div>
+		</div>
+	</div>
+</nav>
+				<div class="Home_content__8iZC8">
+					<div class="Home_container__cVPim vh-100" style="background: url(&quot;assets/img/background.png&quot;) center top / cover no-repeat;">
+						<div class="Home_welcomeContainer__dq5Ei">
+							<div class="Home_welcome__hUwCD container-xl">
+								<div class="Home_welcomeLeft__M7Qlh">
+									<h1>О нас</h1>
+									<p>Мы предоставляем вам лучший клиент для комфортной игры, который даст вам наилучшие впечатления от игры.</p>
+									<div class="Home_welcomeButtons__wxgeb mt-5"> <a class="btn btn-gradient btn-lg mr-2 mt-2" href="authorization.php.html"><i class="fa-solid fa-bag-shopping mr-2" aria-hidden="true"></i>Купить</a> <a class="btn btn-dark btn-lg mt-2" href="https://discord.gg/9XutZb5JZe" target="_blank" rel="noreferrer">Подробнее<i class="fa-solid fa-circle-arrow-right ml-2" aria-hidden="true"></i></a> </div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="Home_container__cVPim">
+						<div class="container-xl">
+							<div class="Home_benefits__eeofC">
+								<div class="Home_benefitsTitle__UVgw0">
+									<h1>Наши преимущества</h1>
+									<p>Мы кратко изложили вам то, что вы гарантированно получите после покупки нашего клиента.</p>
+								</div>
+								<div class="Home_benefit__FkNB8">
+									<div class="Home_benefitHeader__wJVp4"> <span><i class="fa-solid fa-eye mr-2" aria-hidden="true"></i>Красивый внешний вид</span> </div>
+									<div class="Home_benefitBody__FOQOr">
+										<p> В нашем клиенте присутствует большое количество визуальных функций, которые сделают вашу игру более красочной. А так же вы можете настроить оформление клиента под себя, сделав его по настоящему красивым. </p>
+									</div>
+								</div>
+								<div class="Home_benefit__FkNB8">
+									<div class="Home_benefitHeader__wJVp4"> <span><i class="fa-solid fa-sliders mr-2" aria-hidden="true"></i>Настраиваемость</span> </div>
+									<div class="Home_benefitBody__FOQOr">
+										<p> В нашем клиенте вы можете настроить практически любую функцию под себя, а так же использовать конфигурации других пользователей. Благодаря этому вы сможете использовать наш клиент на максимуме его возможностей. </p>
+									</div>
+								</div>
+								<div class="Home_benefit__FkNB8">
+									<div class="Home_benefitHeader__wJVp4"> <span><i class="fa-solid fa-gauge mr-2" aria-hidden="true"></i>Оптимизация</span> </div>
+									<div class="Home_benefitBody__FOQOr">
+										<p>Мы постоянно улучшаем оптимизацию нашего клиента и самой игры. Благодаря этому вы сможете запустить наш клиент даже на слабых компьютерах.</p>
+									</div>
+								</div>
+								<div class="Home_benefit__FkNB8">
+									<div class="Home_benefitHeader__wJVp4"> <span><i class="fa-solid fa-arrow-rotate-right mr-2" aria-hidden="true"></i>Частые обновления</span> </div>
+									<div class="Home_benefitBody__FOQOr">
+										<p>Мы регулярно обновляем функционал в нашем клиенте, добавляя новые функции, а так же совершенствуя уже существующие. Благодаря этому наш клиент имеет обходы под все существующие античиты.</p>
+									</div>
+								</div>
+								<div class="Home_benefit__FkNB8">
+									<div class="Home_benefitHeader__wJVp4"> <span><i class="fa-solid fa-user mr-2"></i>Лучшая поддержка</span> </div>
+									<div class="Home_benefitBody__FOQOr">
+										<p>У нас самая лучшая поддержка, которая разбирается в своём деле и сможет помочь вам по любому вопросу связанному с нашим клиетом.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="Home_container__cVPim">
+						<div class="Home_videoContainer__Ofoc8 container-xl">
+							<h1>Видеообзор</h1>
+							<p>Ниже вы можете посмотреть небольшой геймплей с нашим клиентом.</p>
+							<div class="Home_video__6af+s">
+								<iframe src="https://youtu.be/25tkvbXWzeU?si=a17abrhPFTtk5rGN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen=""></iframe>
+							</div>
+						</div>
+					</div>
+					<div class="Home_container__cVPim">
+						<div class="Home_statisticsContainer__t4cs7 container-xl">
+							<div class="Home_statistics__x9mdl"> <i class="fa-solid fa-users mr-2" aria-hidden="true"></i>
+								<div class="Home_statisticsContent__uwLou"> <span>Пользователей</span>
+									<p>
+										1 276									</p>
+								</div>
+							</div>
+							<div class="Home_statistics__x9mdl"> <i class="fa-solid fa-circle-play mr-2" aria-hidden="true"></i>
+								<div class="Home_statisticsContent__uwLou"> <span>Запусков клиента</span>
+									<p>2 348</p>
+								</div>
+							</div>
+							<div class="Home_statistics__x9mdl"> <i class="fa-solid fa-arrow-rotate-right mr-2" aria-hidden="true"></i>
+								<div class="Home_statisticsContent__uwLou"> <span>Обновлений вышло</span>
+									<p>4</p>
+								</div>
+							</div>
+							<div class="Home_statistics__x9mdl"> <i class="fa-solid fa-calendar-days mr-2" aria-hidden="true"></i>
+								<div class="Home_statisticsContent__uwLou"> <span>Дней с открытия</span>
+									<p>
+										44									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="Footer_footer__u198X">
+    <div class="container-xl">
+        <div class="Footer_logo__IYJVU">
+            <div class="logo mr-2" style="background-image: url(&quot;assets/img/logo.png&quot;);">
+            </div>MANLOXX</div>
+            <div class="Footer_links__05sun">
+                <div class="Footer_leftLinks__wl6Nn">
+                    <span class="mb-2">Навигация</span>
+                    <a class="Footer_link__VojPC" href="index.htm">Главная</a>
+                    <a class="Footer_link__VojPC" href="authorization.php.html">Купить</a>
+                    <a class="Footer_link__VojPC" href="https://discord.gg/9XutZb5JZe" target="_blank" rel="noreferrer">Поддержка</a>
+                </div>
+                    <div class="Footer_centerLinks__C46zx">
+                        <span></span>
+                        <span>ИП Незимов Никита Алексеевич | ИНН 220458953281 | ОГРНИП 324221200052906</span>
+                        <span>Почта для связи: <a href="cdn-cgi/l/email-protection.html" class="__cf_email__" data-cfemail="a1c2d3c0d2c9c5c0ccc8e1d6c8d2d2c4cfc5c2cdc8c4cfd58fd3d4">[email&#160;protected]</a> (ПН-ПТ 10:00-18:00 МСК)</span>
+                    </div>
+                    <div class="Footer_rightLinks__qJx8U"><span class="mb-2">Навигация</span>
+                    <a class="Footer_link__VojPC" href="/datahandle.php">Обработка персональных данных</a>
+                    <a class="Footer_link__VojPC" href="/eula.php">Пользовательское соглашение</a>
+                    <a class="Footer_link__VojPC" href="/rules.php">Правила пользования</a>
+                </div>
+            </div>
+            <div class="Footer_copyright__YcwEz">
+                <span>Copyright © InternalGuard 2024</span>
+            </div>
+        </div>
+    </div>
+</div>		
+	<script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script></body>
+
+	</html>
